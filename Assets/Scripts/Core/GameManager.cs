938731ec975192c7b350d0fe83049508eb586e0a
mob <lsh954325@gmail.com>
2024-06-05 00:42:11 +0900

    근접 적 진행 중

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -4,4 +4,13 @@ using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
+    /// <summary>
+    /// 게임 플레이어
+    /// </summary>
+    [HideInInspector] public Player player;
+
+    protected override void PreInitialize()
+    {
+        player = FindAnyObjectByType<Player>();
+    }
 }
--- Assets/Scripts/Enemy/Enemy_Warrior.cs
+++ Assets/Scripts/Enemy/Enemy_Warrior.cs
@@ -2,15 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Enemy_Warrior : EnemyBase
 {
+    Rigidbody rigid;
+
+    /// <summary>
+    /// 추적 방향 벡터
+    /// </summary>
+    Vector3 dirVec;
+    protected override void Awake()
+    {
+        base.Awake();
+        rigid = GetComponent<Rigidbody>();
+    }
+
     protected override void OnEnable()
     {
         OnReady(2f);
     }
 
+    private void FixedUpdate()
+    {
+        rigid.MovePosition(transform.position + Time.fixedDeltaTime * dirVec * tracingSpeed);
+    }
+
     protected override void OnReady(float delayTime)
     {
         base.OnReady(delayTime);
+
+        traget = FindAnyObjectByType<Player>().gameObject;   // 플레이어로 타겟지정
+    }
+
+    protected override void OnTracing()
+    {
+        dirVec = traget.transform.position - transform.position;
+    }
+
+    protected override void OnAttack()
+    {
+        animator.SetTrigger("Attack");
     }
 }
