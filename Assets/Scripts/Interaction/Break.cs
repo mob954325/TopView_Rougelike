e1011fdcf477ae80b3f387244c9c074a9f08ab2e
mob954325 <lsh954325@gmail.com>
2024-06-13 13:47:33 +0900

    에셋추가

---
+++ Assets/Scripts/Interaction/Break.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Break : MonoBehaviour
+{
+    public void OnBreak()
+    {
+    }
+}
