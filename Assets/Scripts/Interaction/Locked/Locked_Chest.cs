22b3236cc1885ebab032ae80594e1c2774ce3f7e
mob954325 <lsh954325@gmail.com>
2024-06-20 15:03:40 +0900

    상자 아이템 드랍 추가

--- Assets/Scripts/Interaction/Locked/Locked_Chest.cs
+++ Assets/Scripts/Interaction/Locked/Locked_Chest.cs
@@ -7,5 +7,7 @@ public class Locked_Chest : LockedObject
     protected override void AfterOpen()
     {
         // 상자 열고 아이템 소환 
+        Vector3 spawnVector = this.transform.position + transform.up * 1.5f;
+        Factory.Instance.SpawnItem(ItemCodes.Buff_Attack, spawnVector, Quaternion.identity);        
     }
 }
