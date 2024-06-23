ed6b4a3cc59a92f2fff2adbd8ea764a82de88690
mob <lsh954325@gmail.com>
2024-06-23 12:39:12 +0900

    Ability.cs 코드 정리

--- Assets/Scripts/Ability/Ability.cs
+++ Assets/Scripts/Ability/Ability.cs
@@ -50,14 +50,13 @@ public class Ability : MonoBehaviour
         projectile = new GameObject[data.maxCount];
 
         // 최대 개수 만큼 다 만들고 
-        // min개수만 활성화
         for(int i = 0; i < data.maxCount; i++)
         {
             SpawnProjectile(data.code, i);
 
-            if(i < data.minCount)
-            {
-                projectile[i].SetActive(true);  // 첫번째 오브젝트 활성화
+            if(i > data.minCount - 1)   // min개수만 활성화
+            {   
+                projectile[i].SetActive(false); // 나머지 비활성화
             }
         }
     }
@@ -68,68 +67,58 @@ public class Ability : MonoBehaviour
     /// <returns></returns>
     public void AddProjectile()
     {
-        // 추가된 개수 만큼 위치 조정 및 초기화
-        projectile[activeCount].SetActive(true);
-        projectile[activeCount].GetComponent<AbilityObjectBase>().Initialize(rotateSpeed, damage);
+        if (activeCount >= data.maxCount) // 투사체 최대 개수면 무시
+            return;
 
-        // 위치 초기화
-        this.projectile[activeCount].transform.localPosition =
-            new Vector3(this.projectile[activeCount].transform.localPosition.x, 0, this.projectile[activeCount].transform.localPosition.z);
-        activeCount++;
+        projectile[activeCount].SetActive(true);    // 투사체 활성화
+        activeCount++;                              // 활성화 개수 추가
 
-        RefreshProjectile();
+        RefreshAllProjectile();
     }
 
     /// <summary>
-    /// 능력 투사체 생성 함수
+    /// 능력 투사체를 최대개수만큼 생성하고 하는 함수
     /// </summary>
     /// <param name="code">능력 코드</param>
     /// <param name="index">배열 인덱스 값</param>
-    /// <param name="count">몇번째 투사체인지 index값</param>
-    /// <returns></returns>
     void SpawnProjectile(AbilityCode code, int index)
     {
         AbilityObjectBase projectile = Instantiate(data.projectilePrefab, this.transform).GetComponent<AbilityObjectBase>(); // 투사체 추가
 
         if (code == AbilityCode.Rotating)
         {
-            projectile.Initialize(rotateSpeed, damage);      // 오브젝트 생성
-            this.projectile[index] = projectile.gameObject;    // 배열 저장 -> y좌표 0으로 초기화해야함
-
-            this.projectile[index].transform.localPosition =
-                new Vector3(this.projectile[index].transform.localPosition.x, 0, this.projectile[index].transform.localPosition.z); // 투사체 y값 0으로 설정
-            this.projectile[index].SetActive(false);    // 비활성화
+            projectile.Initialize(rotateSpeed, damage);         // 오브젝트 생성
+            this.projectile[index] = projectile.gameObject;     // 배열 저장
         }
 
         activeCount = data.minCount;    // 활성화된 투사체 개수 초기화
 
-        // 투사체 위치
-        float angle = 360f / activeCount;
-
-        Quaternion betweenAngle = Quaternion.Euler(0f, angle * index, 0f);            // 투사체 별 각도
-
-        Vector3 spawnPoint = betweenAngle * Vector3.forward;                                // 위치 계산
-        spawnPoint = new Vector3(spawnPoint.x, 0, spawnPoint.z);
-        this.projectile[index].transform.localPosition = spawnPoint;                        // 위치 조정
+        SetLocalPositionByAngle(index);
     }
 
     /// <summary>
     /// 모든 투사체 위치 초기화 함수
     /// </summary>
-    void RefreshProjectile()
+    void RefreshAllProjectile()
     {
         for(int i = 0; i < projectile.Length; i++)
         {
-            //AbilityObjectBase obj = projectile[i].GetComponent<AbilityObjectBase>();
-
-            // 투사체 위치
-            float angle = 360f / activeCount;
-
-            Quaternion betweenAngle = Quaternion.Euler(0f, angle * i, 0f);            // 투사체 별 각도
-
-            Vector3 spawnPoint = betweenAngle * Vector3.forward;                                // 위치 계산
-            spawnPoint = new Vector3(spawnPoint.x, 0, spawnPoint.z);
-            this.projectile[i].transform.localPosition = spawnPoint;                            // 위치 조정
+            SetLocalPositionByAngle(i);
         }
+    }
+    /// <summary>
+    /// 각도에 따라 투사체 위치를 설정하는 함수
+    /// </summary>
+    /// <param name="index">투사체 인덱스</param>
+    void SetLocalPositionByAngle(int index)
+    {
+        // 투사체 위치
+        float angle = 360f / activeCount;
+
+        Quaternion betweenAngle = Quaternion.Euler(0f, angle * index, 0f);            // 투사체 별 각도
+
+        Vector3 spawnPoint = betweenAngle * Vector3.forward;                          // 위치 계산
+        spawnPoint = new Vector3(spawnPoint.x, 0, spawnPoint.z);
+        this.projectile[index].transform.localPosition = spawnPoint;                  // 위치 조정
     }
 }
